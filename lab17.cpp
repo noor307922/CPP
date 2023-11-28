c084681b591ad4336b4e374b7c3162346273a995
noor307922 <125033135+noor307922@users.noreply.github.com>
2023-11-28 23:51:05 +0530

    Update lab17.cpp

--- lab17.cpp
+++ lab17.cpp
@@ -36,7 +36,7 @@ void bubbleSort(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {64, 25, 12, 22, 11};
+    int arr[] = {64, 45, 32, 23, 13};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     selectionSort(arr, n);
